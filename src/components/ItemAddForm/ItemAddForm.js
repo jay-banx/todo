647a19a43893474060df4c9deec35704b24ba72e
Golovchenko Denis <golovchenko.dennis@gmail.com>
2020-03-29 17:51:38 +0300

    Added ItemAddForm

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./ItemAddForm.css";
 
-const ItemAddForm = ({ onAdded }) => {
-  return (
-    <div className="item-add-form">
-      <button
-        type="button"
-        className="btn btn-outline-primary float-right"
-        onClick={() => onAdded("Hello world")}
-      >
-        Add Item
-      </button>
-    </div>
-  );
-};
+class ItemAddForm extends Component {
+  state = {
+    label: ""
+  };
+
+  onChangeLabel = event => {
+    this.setState({
+      label: event.target.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    const label = this.state.label.trim();
+    if (label !== "") {
+      this.props.onAdded(label);
+      this.setState({
+        label: ""
+      });
+    }
+  };
+
+  render() {
+    return (
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Type new todo"
+          value={this.state.label}
+          onChange={this.onChangeLabel}
+        />
+        <input
+          type="submit"
+          value="Add todo"
+          className="btn btn-outline-primary"
+        />
+      </form>
+    );
+  }
+}
 
 export default ItemAddForm;
